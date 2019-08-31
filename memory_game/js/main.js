fbf440e6c88928efb45777c516e165b201ea35a8
Irene Tokareva <longhorn09@gmail.com>
2019-08-31 18:53:10 -0500

    updated game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,10 +1,20 @@
 console.log("It works!");
 
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
 console.log("User flipped " + cardOne);
-console.log("User flipped ") + cardThree);
+
+var cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2) {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again.")
+  }
+}
