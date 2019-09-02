6079985f843d5a14c769242da358a8ae3bf5db15
Irene Tokareva <longhorn09@gmail.com>
2019-09-02 15:37:43 -0500

    updated with event listeners

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -22,23 +22,40 @@ var cards = [
     cardImage : "images/king-of-diamonds.png"
   }
 ];
+
 var cardsInPlay = [];
+var score = 0;
+
+
 
 function checkForMatch() {
+
   if (cardsInPlay[0] === cardsInPlay[1]) {
-    console.log("You found a match!");
+    score++;
+    alert("You found a match!");
   } else {
-    console.log("Sorry, try again.");
-  }
-}
+    alert("Sorry, try again.");
+  };
+  document.getElementById("score").innerHTML = "Your score is " + score;
+};
 
-function flipCard(cardId){
 
-  console.log("User flipped " + cards[cardId].rank);
-  console.log("User flipped " + cards[cardId].suit);
-  console.log("User flipped " + cards[cardId].cardImage);
+
+console.log(score);
+
+
+var flipCard = function() {
+
+  let cardId = this.getAttribute("data-id");
+  console.log(this);
+
+  // console.log("User flipped " + cards[cardId].rank);
+  // console.log("User flipped " + cards[cardId].suit);
+  // console.log("User flipped " + cards[cardId].cardImage);
 
   cardsInPlay.push(cards[cardId].rank);
+
+  this.setAttribute("src", cards[cardId].cardImage);
 
   if (cardsInPlay.length === 2) {
     checkForMatch();
@@ -46,5 +63,29 @@ function flipCard(cardId){
 
 };
 
-flipCard(0);
-flipCard(2);
+var createBoard = function() {
+  console.log("createBoard called with " + cards.length  + " length");
+  for (let i = 0; i < cards.length; i++) {
+
+    var cardElement = document.createElement("img");
+    cardElement.setAttribute("src","images/back.png");
+    cardElement.setAttribute("data-id",i);
+    cardElement.addEventListener("click", flipCard);
+    document.getElementById("game-board").appendChild(cardElement);
+  }
+  document.getElementById("score").innerHTML = "Your score is " + score;
+};
+
+console.log(document.getElementById("game-board"));
+
+createBoard();
+
+var resetBoard = function() {
+  cardsInPlay = [];
+  console.log("createBoard called with " + cards.length  + " length");
+  document.getElementById("game-board").innerHTML = "";
+  createBoard();
+
+};
+
+document.getElementById("reset").addEventListener("click", resetBoard);
